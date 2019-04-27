254a4b1b203e6cd155e3a9602a9ba065fab046b2
evilcoder <rajatsri94@gmail.com>
2019-04-27 10:49:35 +0530

    Setup pypi package

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='search-torrent',
+    description='Search for torrents using command line',
+    version='1.0.0',
+    url='http://github.com/rajat19/torrent-crawler',
+    download_url='https://github.com/rajat19/torrent-crawler/releases',
+    author='Rajat Srivastava',
+    author_email='rajatsri94@gmail.com',
+    long_description=long_description,
+    packages=find_packages(),
+    keywords=['pip', 'torrent', 'crawler'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'search-torrent=torrent_crawler.search:main',
+        ],
+    },
+    install_requires=[
+        'bs4',
+        'requests',
+    ]
+)
